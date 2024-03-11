338358a645e07d4534cc3fcddaa1b56f01ddc9e8
Adrian Jeledintan <adrian.jeledintan@gmail.com>
2024-03-11 15:02:01 +0200

    fix CreatedTrait ModifiedTrait for doctrine orm 3

--- src/Entity/CreatedTrait.php
+++ src/Entity/CreatedTrait.php
@@ -13,9 +13,7 @@ use Doctrine\ORM\Mapping as ORM;
 
 trait CreatedTrait
 {
-    /**
-     * @ORM\Column(type="datetime", nullable=true)
-     */
+    #[ORM\Column(type: 'datetime', nullable: true)]
     private ?DateTime $created;
 
     public function getCreated(): ?DateTime
--- src/Entity/ModifiedTrait.php
+++ src/Entity/ModifiedTrait.php
@@ -13,9 +13,7 @@ use Doctrine\ORM\Mapping as ORM;
 
 trait ModifiedTrait
 {
-    /**
-     * @ORM\Column(type="datetime", nullable=false, options={"default"="CURRENT_TIMESTAMP", "update"=true})
-     */
+    #[ORM\Column(type: 'datetime', nullable: false, options: ['default' => 'CURRENT_TIMESTAMP', 'update' => true])]
     private DateTime $modified;
 
     public function getModified(): ?DateTime
